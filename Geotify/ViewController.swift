3bf7e55bb3c40f4925355a8907b4fe51957cd007
William Breen <wbreen@mooney202-17.elon.edu>
2016-10-19 15:07:21 -0400

    first commit, created UI

---
+++ Geotify/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Geotify
+//
+//  Created by William Breen on 10/19/16.
+//  Copyright © 2016 William Breen. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
